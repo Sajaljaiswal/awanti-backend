7cca03b3c2c5a3a150e7b1ad823c90e93fa13bff
vikasvj <vikasvj91221@gmail.com>
2026-02-04 12:16:11 +0530

    added ticket status routes

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,20 +1,42 @@
 import { supabase } from "../src/supabase.js";
 
 const authMiddleware = async (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) return res.status(401).json({ message: "No token" });
 
-  if (!token) {
-    return res.status(401).json({ message: "Token missing" });
+    const token = authHeader.split(" ")[1];
+    if (!token) return res.status(401).json({ message: "Invalid token format" });
+
+    // 1️⃣ Verify token (Auth data)
+    const { data: authData, error: authError } = await supabase.auth.getUser(token);
+
+    if (authError || !authData?.user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const user = authData.user;
+
+    const { data: profile } = await supabase
+      .from("profiles") 
+      .select("name, role")
+      .eq("id", user.id)
+      .single();
+
+    // 3️⃣ Attach user to request (With Name)
+    req.user = {
+      id: user.id,
+      email: user.email,
+      name: profile?.name  || user.email.split('@')[0],
+      role: profile?.role || user.user_metadata?.role || "user"
+    };
+
+    next();
+
+  } catch (err) {
+    console.error("Auth Error:", err);
+    res.status(500).json({ message: "Auth failed" });
   }
-
-  const { data, error } = await supabase.auth.getUser(token);
-
-  if (error) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
-
-  req.user = data.user;
-  next();
 };
 
 export default authMiddleware;
--- routes/tickets.js
+++ routes/tickets.js
@@ -8,6 +8,9 @@ import {
   updateTicket,
   updateTicketStatus,
   assignTicket,
+  getLatestStatus,
+  getTicketDetails
+  
 } from "../controllers/ticketController.js";
 
 const router = express.Router();
@@ -16,11 +19,16 @@ const router = express.Router();
 router.post("/", authMiddleware, requireRole(["admin"]), createTicket);
 router.delete("/:id", authMiddleware, requireRole(["admin"]), deleteTicket);
 router.put("/:id", authMiddleware, requireRole(["admin"]), updateTicket);
-
+router.get("/allstatus",authMiddleware,requireRole(["admin"]),getLatestStatus);
+router.get("/:id/details",authMiddleware,requireRole(["admin"]), getTicketDetails);
 /* ADMIN + STAFF */
 router.get("/", authMiddleware, getAllTickets); 
-router.patch("/:id/status", authMiddleware, updateTicketStatus);
+// router.patch("/:id/status", authMiddleware, updateTicketStatus);
 router.put("/:id/assign", authMiddleware, requireRole(["admin"]), assignTicket);
 
-
+// routes/ticketRoutes.js
+router.patch("/:id/status", authMiddleware, (req, res, next) => {
+    console.log("ROUTE HIT HO GAYA! ID:", req.params.id);
+    next();
+}, updateTicketStatus);
 export default router;
